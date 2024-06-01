d1d20cb39d9bf4b6f1d8347cb47459650f8e0cd8
Moarz Naing Lin Khant <nainglinkhant2005@gmail.com>
2024-06-02 05:44:54 +0800

    Basic declaration, traverse, add

--- linkedList.py
+++ linkedList.py
@@ -1,1 +1,43 @@
-print("Hello Linked List")
+class Node:
+    def __init__(self, data):
+        self.data = data 
+        self.next = None 
+        
+class linkedList: 
+    def __init__(self): 
+        self.head = None 
+    
+    def add(self, data): 
+        
+        newNode = Node(data) 
+        
+        if self.head is None: 
+            self.head = newNode 
+            return 
+        
+        last = self.head 
+        while last.next is not None: 
+            last = last.next 
+        last.next = newNode          
+        
+    def traverse(self): 
+        currentNode = self.head 
+        
+        while currentNode is not None: 
+            print(currentNode.data, end=" -> " ) 
+            currentNode = currentNode.next 
+        print("None")
+
+
+       
+            
+mylinkedlist = linkedList() 
+mylinkedlist.add("A")
+mylinkedlist.add("B")
+mylinkedlist.add("C")
+
+mylinkedlist.traverse()
+
+
+ 
+  
