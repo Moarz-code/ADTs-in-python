7a872c155d1963ec578e267e1d058ae0ec51939a
Moarz Naing Lin Khant <nainglinkhant2005@gmail.com>
2024-06-02 06:06:23 +0800

    display binary tree in its original form, not my code

--- binaryTree.py
+++ binaryTree.py
@@ -2,7 +2,7 @@ class Node:
     def __init__(self): 
         self.left = None 
         self.right = None 
-        self.val = data 
+        self.data = data 
     
     def insert(self, newData): 
         if self.root is None: 
@@ -12,7 +12,66 @@ class Node:
                 
     def _insert(self, root, newData): 
         if newData > root.data: 
-            root.left = Node(key)
-        else:
+            if root is None: 
+                self.root = Node(key)
+            else: 
+                self._insert(root.left, key)    
+        else: 
+            if root.right is None: 
+                root.right = Node(key)
+            else: 
+                self._insert(root.right, key) 
+    def print_tree(self):
+        lines, *_ = self._display_aux(self.root)
+        for line in lines:
+            print(line)
+
+    def _display_aux(self, node):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if node.right is None and node.left is None:
+            line = '%s' % node.val
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if node.right is None:
+            lines, n, p, x = self._display_aux(node.left)
+            s = '%s' % node.val
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if node.left is None:
+            lines, n, p, x = self._display_aux(node.right)
+            s = '%s' % node.val
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self._display_aux(node.left)
+        right, m, q, y = self._display_aux(node.right)
+        s = '%s' % node.val
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+              
+                 
+                
             
         
